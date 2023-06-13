d37485514b9e84c4229dc1ab0a4f49d3821d0847
“vikyyady” <vikyady1@gmail.com>
2023-06-13 09:47:51 +0800

    Menjalankan Program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -2,6 +2,9 @@ package universitas;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
+        Mahasiswa viky = new Mahasiswa("2110010210", "Viky Adi Arya Efendi", "Teknik Informatika");
+        
+        System.out.println(viky.getDetail());
 
     }
 }
